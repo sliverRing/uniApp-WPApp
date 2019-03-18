f7d04dd8159ff062378a988c1dd4ef4e4849dbcc
“YouSliver_ring <lqczzyx@sina.com>
2019-03-18 08:14:04 +0800

    修改pages>index/index.vue
    增加幻灯片
    
    Signed-off-by: “YouSliver_ring <lqczzyx@sina.com>

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,35 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
+    <view>
+        <view class="uni-padding-wrap">
+            <view class="page-section swiper">
+                <view class="page-section-spacing">
+				    <!-- 一组幻灯片代码开始，用到组件swiper -->
+					<!-- indicator-dots autoplay interval……：组件相关属性，具体可以查看官网说明 -->
+                    <swiper class="swiper" indicator-dots="indicatorDots" autoplay="autoplay" interval="interval" duration="duration">
+                        <!-- 每组幻灯片中的子项目 1 -->
+						<swiper-item>
+							<!-- 此处放内容，可以是图片，也可是图片加文字 -->
+                        </swiper-item>
+						 <!-- 每组幻灯片中的子项目 2 -->
+                        <swiper-item>
+							<!-- 此处放内容，可以是图片，也可是图片加文字 -->
+                        </swiper-item>
+						 <!-- 每组幻灯片中的子项目 3 -->
+                        <swiper-item>
+							<!-- 此处放内容，可以是图片，也可是图片加文字 -->
+                        </swiper-item>
+                    </swiper>
+                </view>
+            </view>
         </view>
-	</view>
+    </view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
 		onLoad() {
@@ -24,17 +42,8 @@
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+	/* 将这组幻灯片中的子项目改成我们设计图中的灰色 */
+	swiper-item{
+		background-color: #f8f8f8;
 	}
 </style>
